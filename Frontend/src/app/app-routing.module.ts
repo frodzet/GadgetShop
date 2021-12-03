70ba7ec6d12ace782cd181c2966469c43a2c63c9
frodzet <simon.birkedal@gmail.com>
2021-12-03 12:54:05 +0100

    Second Commit

---
+++ Frontend/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'pets',
+    loadChildren: () => import('./admin-pets/admin-pets.module')
+      .then(m => m.AdminPetsModule)
+  },
+  {
+    path: 'todos',
+    loadChildren: () => import('./todos/todos.module')
+      .then(m => m.TodosModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
